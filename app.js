378676bfcbf6de042ab85296b580405b2597820d
Anon <heyyyrofl@gmail.com>
2020-10-18 18:41:08 -0400

    Cleaned up code

--- app.js
+++ app.js
@@ -10,54 +10,37 @@ client.once('ready', () => {
 
 client.login('NzY3NDIxMTU3OTUxMjc1MDA4.X4xqpg.h-diN6oX8v0jrrszU7ND74PWRxY');
 
-client.on('message', message => {
-    if (message.content === '!serverstatus') {
+const commands = {
+    "!serverstatus":
+    {
+        "function": function(message){
+            (async function(){
+                try {
+                    const browser = await puppeteer.launch();
+                    const [page] = await browser.pages();
+            
+                    await page.goto('https://www.xenrebirth.com/index.php', { waitUntil: 'networkidle0' });
+            
+                    const index_data = await page.evaluate(() => [
+                        document.querySelectorAll('.srv-box2')[0].textContent,
+                        document.querySelectorAll('.srv-box2')[1].textContent,
+                    ]);
 
-        (async function main() {
-            try {
-                const browser = await puppeteer.launch();
-                const [page] = await browser.pages();
-        
-                await page.goto('https://www.xenrebirth.com/index.php', { waitUntil: 'networkidle0' });
-        
-                const index_data = await page.evaluate(() => [
-                    document.querySelectorAll('.srv-box2')[0].textContent,
-                    document.querySelectorAll('.srv-box2')[1].textContent,
-                ]);
+                    await browser.close();
+                    message.channel.send('*' + index_data[0] + "* (" + index_data[1] + " Active)");
 
-                message.channel.send(index_data[0] + " (" + index_data[1] + " Active)");
-
-                await browser.close();
-            } catch (err) {
-                console.error(err);
-            }
-        })();
-    } 
-    else if(message.content === '!worldboss'){
-
-        (async function main() {
-            try {
-                const browser = await puppeteer.launch();
-                const [page] = await browser.pages();
-        
-                await page.goto('https://www.xenrebirth.com/index.php?game-bosstimer/', { waitUntil: 'networkidle0' });
-        
-                const worldboss_data = await page.evaluate(() => [
-                    document.querySelectorAll('#event-listing .bossRow .event-name strong')[0].textContent,
-                    document.querySelectorAll('#event-listing .bossRow .columnTime .event-ctimer')[0].textContent,
-                ]);
-
-                message.channel.send(worldboss_data[0] + ' in ' + worldboss_data[1]);
-
-                await browser.close();
-            } catch (err) {
-                console.error(err);
-            }
-        })();
-    }
-    else if(message.content === '!worldbossalerts'){
-        var interval = setInterval (function () {
-            (async function main() {
+                } catch (err) {
+                    console.error(err);
+                }
+            })();
+        },
+        "description": "Returns the server status and number of users online.",
+        "type": "single"
+    },
+    "!worldboss":
+    {
+        "function": function(message){
+            return (async function(){
                 try {
                     const browser = await puppeteer.launch();
                     const [page] = await browser.pages();
@@ -69,18 +52,133 @@ client.on('message', message => {
                         document.querySelectorAll('#event-listing .bossRow .columnTime .event-ctimer')[0].textContent,
                     ]);
 
-                    var string = worldboss_data[1];
-                    string = string.split(/[ ,]+/);
-                    
-                    if(string[1] === '10m'){
-                        message.channel.send(worldboss_data[0] + ' in ' + string[1]);
-                    }
-    
                     await browser.close();
+                    message.channel.send('*' + worldboss_data[0] + '* in __' + worldboss_data[1] + '__');
+
                 } catch (err) {
                     console.error(err);
                 }
             })();
-        }, 60000);  
+        },
+        "description": "Returns the next world boss' name and spawn time.",
+        "type": "single"
+    },
+    "!worldbossalerts":
+    {
+        "function": function(message){
+            var interval = setInterval (function () {
+                (async function(){
+                    try {
+                        const browser = await puppeteer.launch();
+                        const [page] = await browser.pages();
+                
+                        await page.goto('https://www.xenrebirth.com/index.php?game-bosstimer/', { waitUntil: 'networkidle0' });
+                
+                        const worldboss_data = await page.evaluate(() => [
+                            document.querySelectorAll('#event-listing .bossRow .event-name strong')[0].textContent,
+                            document.querySelectorAll('#event-listing .bossRow .columnTime .event-ctimer')[0].textContent,
+                        ]);
+
+                        var string = worldboss_data[1];
+                        string = string.split(/[ ,]+/);
+                        
+                        if(string[1] === '10m'){
+                            message.channel.send('*' + worldboss_data[0] + '* in __' + string[1] + '__');
+                        }
+
+                        await browser.close();
+                    } catch (err) {
+                        console.error(err);
+                    }
+                })();
+            }, 60000);
+            message.channel.send('Now alerting channel 10 minutes before world bosses spawn.');
+        },
+        "description": "Begins alerting world boss notification whenever there is 10 minutes left til spawn.",
+        "type": "looping"
+    },
+    "!next":
+    {
+        "function": function(message){
+            return (async function(){
+                try {
+                    const browser = await puppeteer.launch();
+                    const [page] = await browser.pages();
+            
+                    await page.goto('https://www.xenrebirth.com/index.php?game-bosstimer/', { waitUntil: 'networkidle0' });
+            
+                    const worldboss_data = await page.evaluate(() => [
+                        document.querySelectorAll('#event-listing .bossRow .event-name strong')[1].textContent,
+                        document.querySelectorAll('#event-listing .bossRow .columnTime .event-ctimer')[1].textContent,
+                        document.querySelectorAll('#event-listing .bossRow .event-name strong')[2].textContent,
+                        document.querySelectorAll('#event-listing .bossRow .columnTime .event-ctimer')[2].textContent,
+                    ]);
+
+                    await browser.close();
+                    message.channel.send('*' + worldboss_data[0] + '* in __' + worldboss_data[1] + '__\n*' + worldboss_data[2] + '* in __' + worldboss_data[3] + '__');
+
+                } catch (err) {
+                    console.error(err);
+                }
+            })();
+        },
+        "description": "Returns the next 2 world bosses.",
+        "type": "single"
+    },
+    "!servertime":
+    {
+        "function": function(message){
+            (async function(){
+                try {
+                    const browser = await puppeteer.launch();
+                    const [page] = await browser.pages();
+            
+                    await page.goto('https://www.xenrebirth.com/index.php?game-bosstimer/', { waitUntil: 'networkidle0' });
+            
+                    const worldboss_data = await page.evaluate(() => [
+                        document.querySelectorAll('#event-servertime-c')[0].textContent,
+                    ]);
+
+                    await browser.close();
+                    message.channel.send(worldboss_data[0]);
+
+                } catch (err) {
+                    console.error(err);
+                }
+            })();
+        },
+        "description": "Returns the server status and number of users online.",
+        "type": "single"
+    },
+};
+
+client.on('message', message => {
+    if(message.content === '!commands'){
+
+        full_msg = "";
+        sortable = [];
+
+        for (var key in commands) {
+            // skip loop if the property is from prototype
+            if (!commands.hasOwnProperty(key)) continue;
+
+            var obj = commands[key];
+            sortable.push(key);
+        }
+
+        sortable.sort(function(a, b) {
+            var x = a.toLowerCase();
+            var y = b.toLowerCase();
+            return x < y ? -1 : x > y ? 1 : 0;
+        });
+
+        sortable.forEach(function(entry) {
+            full_msg += "__" + entry + "__: " + commands[entry]["description"] + '\n';
+        });
+
+        message.channel.send(full_msg);
     }
+	else if(message.content in commands){
+        commands[message.content]["function"](message);
+	}
 });
